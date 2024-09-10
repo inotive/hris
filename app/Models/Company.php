b7288b90a47ff6ee04c05b97c82466cc98d8ca41
Bayu Lukman Yusuf <bayuly94@gmail.com>
2024-09-10 09:41:20 +0700

    create companies table

---
+++ app/Models/Company.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    use HasFactory;
+    use HasUuids;
+
+
+    protected $primaryKey = 'id'; // Use 'id' as the primary key
+    public $incrementing = false;  // Disable auto-incrementing
+    protected $keyType = 'string'; // Since UUID is a string
+}
