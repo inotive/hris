ed9d7dfac147c8f6e5b2ae572509c6856399a092
Bayu Lukman Yusuf <bayuly94@gmail.com>
2024-09-19 15:30:15 +0700

    bug fix

--- app/Traits/CrudTrait.php
+++ app/Traits/CrudTrait.php
@@ -14,9 +14,7 @@ trait CrudTrait
 
     public function index(Request $request)
     {
-        $rows_count = $this->model::when(auth()->user()->company_id != null, function($query){
-            $query->where('company_id', auth()->user()->company_id);
-        })->count();
+        $rows_count = $this->model::count();
 
         if ($request->generate_dummy == 1) {
          
