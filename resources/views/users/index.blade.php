0bbe9fa96e9e0ea479e6c51277a79f05c52a370e
Agung Kurniawan <agung41149@gmail.com>
2024-09-25 22:20:44 +0700

    - add style for view user, employee, companies on table list

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,9 +3,8 @@
 
 
 @section('table_header')
-    <th class="min-w-125px">{{ __('Image') }}</th>
-    <th class="min-w-125px">{{ __('First Name') }}</th>
-    <th class="min-w-125px">{{ __('Last Name') }}</th>
+    <th class="min-w-50px">{{ __('No') }}</th>
+    <th class="min-w-125px">{{ __('User') }}</th>
     <th class="min-w-125px">{{ __('Email') }}</th>
     <th class="min-w-125px">{{ __('Phone') }}</th>
     <th class="min-w-125px">{{ __('Role') }}</th>
@@ -18,12 +17,16 @@
 @section('table_body')
     @foreach ($list as $key => $value)
         <tr>
+            <td>{{ ($list->currentPage() - 1) * $list->perPage() + $key + 1 }}</td>
             <td>
-                <img class="rounded" src="{{ Storage::url($value->image) }}"
-                onerror="this.onerror=null; this.src='{{ asset('assets/images/no_image.jpg') }}';" width="50" />
+                <div class="d-flex align-items-center">
+                    <img class="rounded" src="{{ Storage::url($value->image) }}"
+                    onerror="this.onerror=null; this.src='{{ asset('assets/images/no_image.jpg') }}';" width="50" />
+                    <div class="ms-5">
+                        <b>{{ $value->first_name }} {{ $value->last_name }}</b>
+                    </div>
+                </div>
             </td>
-            <td>{{ $value->first_name }}</td>
-            <td>{{ $value->last_name }}</td>
             <td>{{ $value->email }}</td>
             <td>{{ $value->phone ?? '-' }}</td>
             <td>{{ $value->role_label() }}</td>
