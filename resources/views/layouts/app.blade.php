f4fe2940a590ae29cb007799b936b3c4649ab624
Agung Kurniawan <agung41149@gmail.com>
2024-09-26 14:26:05 +0700

    - add title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-    <title>{{ config("app.name", "PeopleIn | #1 HR System for your company") }}</title>
+    <title>PeopleIn | #1 HR System for your company</title>
     <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
 
     <!-- Fonts -->
