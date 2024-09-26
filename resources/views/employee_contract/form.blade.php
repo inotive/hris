7e6e3f92f83446c2a887f89a2a5793e32c7b9fbc
Agung Kurniawan <agung41149@gmail.com>
2024-09-26 14:00:37 +0700

    - add logo for sidebar - fix label on contract form

--- resources/views/employee_contract/form.blade.php
+++ resources/views/employee_contract/form.blade.php
@@ -2,7 +2,7 @@
 
     <x-form.hidden name="employee_id" :value="old('employee_id', $employee->id ?? '')" />
 
-    <x-form.select name="status" :value="old('status', $employee->status ?? '')" :list="['contract'=> 'Contract', 'permanent'=>'Permanent']" />
+    <x-form.select :label="__('Type')" name="status" :value="old('status', $employee->status ?? '')" :list="['contract'=> 'Contract', 'permanent'=>'Permanent']" />
 
     <x-form.datepicker :label="__('Date Start')" name="date_start" :value="$form->date_start ?? ''" />
     <x-form.datepicker :label="__('Date End')" name="date_end" :value="$form->date_end ?? ''" />
