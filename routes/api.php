8e3ef6c4ecb55928600bfefee5eca1d5869f33e9
Bayu Lukman Yusuf <bayuly94@gmail.com>
2024-10-28 00:28:38 +0700

    fix route

--- routes/api.php
+++ routes/api.php
@@ -95,7 +95,7 @@ Route::middleware([
     Route::get('/master/reason-leaving', [LeaveRequestController::class, 'reasonLeaving']);
     Route::get('/master/leave-type', [LeaveRequestController::class, 'leaveType']);
     Route::get('/master/overtime-type', [OvertimeRequestController::class, 'overtimeType']);
-    Route::get('/master/reimburesement-type', [ReimbursementController::class, 'reimburesementType']);
+    Route::get('/master/reimbursement-type', [ReimbursementController::class, 'reimburesementType']);
     Route::get('/master/expense-type', [ReimbursementController::class, 'expenseType']);
     Route::get('/master/months', [AttendanceController::class, 'monthList']);
     Route::get('/master/years', [AttendanceController::class, 'yearList']);
