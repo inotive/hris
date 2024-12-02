b701fc0c67b117a31187f69a3182464c30f6ca58
Bayu Lukman Yusuf <bayuly94@gmail.com>
2024-12-02 13:40:07 +0700

    update

--- resources/views/icons/eye.blade.php
+++ resources/views/icons/eye.blade.php
@@ -1,4 +1,4 @@
-<?xml version="1.0" encoding="utf-8"?>
+
 <svg width="32px" height="32px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g clip-path="url(#clip0_15_200)">
 <rect width="24" height="24" fill="white"/>
