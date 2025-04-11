2ce2b1f42c4ebeec39adfa0d4444248aa7148fa3
Bayu Lukman Yusuf <bayuly94@gmail.com>
2025-04-11 09:24:11 +0700

    Fix model reference in CompanySubscriptionController to use CompanySubscription

--- app/Http/Controllers/CompanySubscriptionController.php
+++ app/Http/Controllers/CompanySubscriptionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Banner;
 use App\Models\Company;
 use App\Models\CompanySubscription;
+use App\Models\CompanySubscriptionGroupCompany;
 use App\Traits\CrudTrait;
 use Illuminate\Http\Request;
 
@@ -12,7 +13,7 @@ class CompanySubscriptionController extends Controller
 {
     use CrudTrait;
 
-    public $model = Company::class;
+    public $model = CompanySubscription::class;
     public $route = 'company-subscriptions';
     public $page_title = 'Company Subscriptions';
     public $action_title = 'Company Subscription';
