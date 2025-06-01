d1264daca4bac7c38bad9b552553dc1b4fdb5a7f
Bayu Lukman Yusuf <bayuly94@gmail.com>
2025-06-02 03:46:59 +0700

    handle nullable active_subscriptions

--- app/Http/Middleware/CompanySubscriptionCheck.php
+++ app/Http/Middleware/CompanySubscriptionCheck.php
@@ -5,6 +5,8 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Exception;
+use Illuminate\Support\Facades\Log;
 
 class CompanySubscriptionCheck
 {
@@ -17,22 +19,36 @@ class CompanySubscriptionCheck
      */
     public function handle(Request $request, Closure $next)
     {
-        $user = auth()->user();
-        if ($user->role != 'superadmin') {
-            $active = $user->company->active_subscriptions();
-            if (count($active) <= 0) {
-//                return redirect()->route('no-active-subscription');
+        try{
+            $user = auth()->user();
+            if ($user->role != 'superadmin') {
+                $active = $user->company->active_subscriptions() ?? [];
+                if (count($active) <= 0) {
+    //                return redirect()->route('no-active-subscription');
 
-                Auth::logout();
-                $request->session()->invalidate();
-                $request->session()->regenerateToken();
+                    Auth::logout();
+                    $request->session()->invalidate();
+                    $request->session()->regenerateToken();
 
 
-                $message = __('dashboard.no_active_subscription');
+                    $message = __('dashboard.no_active_subscription');
 
-                return redirect('/login?message=' . $message);
+                    return redirect('/login?message=' . $message);
+                }
             }
+
+            return $next($request);
+        }catch(Exception $e){
+            
+            Log::info($e->getMessage());
+
+            $message = __('dashboard.no_active_subscription');
+
+            return redirect('/login?message=' . $message);
+
+
+            return $next($request);
         }
-        return $next($request);
+   
     }
 }
