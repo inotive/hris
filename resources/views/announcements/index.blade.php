58241bde55c0d158c32f1829a4691b29c16af782
FakhrizalN <fakhrizal577@gmail.com>
2026-02-19 11:13:54 +0800

    Fix Announcement Table

--- resources/views/announcements/index.blade.php
+++ resources/views/announcements/index.blade.php
@@ -2,21 +2,21 @@
 
 
 @section('page_title')
-    {{ __($page_title) }}
+{{ __($page_title) }}
 @stop
 
 @section('toolbar')
-    <x-table.filter-dropdown :company="true" />
+<x-table.filter-dropdown :company="true" />
 @stop
 
 @section('table_header')
-    <th class="min-w-10px">{{ __('ID') }}</th>
-    @if (auth()->user()->company_id == null)
-        <th class="min-w-125px">{{ __('Company') }}</th>
-    @endif
-    <th class="min-w-125px">{{ __('Image') }}</th>
-    <th class="min-w-125px">{{ __('Title') }}</th>
-    <th class="text-end min-w-70px">{{ __('Action') }}</th>
+<th class="min-w-10px">{{ __('ID') }}</th>
+@if (auth()->user()->company_id == null)
+    <th class="min-w-125px">{{ __('Company') }}</th>
+@endif
+
+<th class="min-w-125px">{{ __('Title') }}</th>
+<th class="text-end min-w-70px">{{ __('Action') }}</th>
 @stop
 
 @section('table_body')
@@ -27,10 +27,7 @@
             @if (auth()->user()->company_id == null)
                 <td>{{ $value->company->name ?? '-' }}</td>
             @endif
-            <td>
-                <img class="rounded" src="{{ Storage::url($value->image) }}"
-                    onerror="this.onerror=null; this.src='{{ asset('assets/images/no_image.jpg') }}';" width="50" />
-            </td>
+
             <td>{{ $value->title ?? '-' }}</td>
             <td class="text-end">
                 <x-table.actions>
